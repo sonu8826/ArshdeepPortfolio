b23c665afb1876b923740cc895e61602262ece63
Sonu Thakur <sonu88ggn@gmail.com>
2024-08-23 21:24:03 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,81 @@
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
+
+function sendMail () {
+    var params = {
+        name : document.getElementById("name").value ,
+        email : document.getElementById("email").value ,
+        contact : document.getElementById("contact").value ,
+        message : document.getElementById("message").value ,
+    };
+
+const serviceID = "service_fkwlr3h";
+const templateID = "template_nups9th";
+
+emailjs.send(serviceID,templateID, params).then((res) => {
+    document.getElementById("name").value ="";
+    document.getElementById("email").value ="";
+    document.getElementById("contact").value ="";
+    document.getElementById("message").value ="";
+    console.log(res)
+    alert("Your Message Sent Succesfully");
+}).catch((err) =>{
+    console.log(err)
+})
+}
+
+
